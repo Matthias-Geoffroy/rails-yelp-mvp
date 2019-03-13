644cc0a27795d10d2270b5efc24ab5d192554f9f
Matthias Geoffroy <matthiasgeoffroy@gmail.com>
2019-03-13 12:59:03 +0100

    Add validations to restaurant and review
    Signed-off-by: Matthias Geoffroy <matthiasgeoffroy@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_112257) do
+ActiveRecord::Schema.define(version: 2019_03_13_114448) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
