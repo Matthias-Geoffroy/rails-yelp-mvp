ff79d0f8811bca4f1f51e294d0b540168aa67c71
Matthias Geoffroy <matthiasgeoffroy@gmail.com>
2019-03-13 12:27:24 +0100

    add column address to restaurants
    Signed-off-by: Matthias Geoffroy <matthiasgeoffroy@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_105448) do
+ActiveRecord::Schema.define(version: 2019_03_13_112257) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_13_105448) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "reviews", force: :cascade do |t|
