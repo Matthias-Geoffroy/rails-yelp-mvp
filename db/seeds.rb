1194670c164c8e316a74248536ebd11f26d9ec28
Matthias Geoffroy <matthiasgeoffroy@gmail.com>
2019-03-13 13:09:10 +0100

    Seeds added
    Signed-off-by: Matthias Geoffroy <matthiasgeoffroy@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:    'La Cigale',
+    address: 'Nantes',
+    category: "french"
+  },
+  {
+    name:    'O Bistrot Quai',
+    address: 'Nantes',
+    category: "french"
+  },
+  {
+    name:    'Esprit Tchai',
+    address: 'Paris',
+    category: "chinese"
+  },
+  {
+    name:    'Le 1',
+    address: 'Nantes',
+    category: "french"
+  },
+  {
+    name:    'Creperie',
+    address: 'Nantes',
+    category: "french"
+  },
+  {
+    name:    'Italien resto',
+    address: 'Paris',
+    category: "italian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
